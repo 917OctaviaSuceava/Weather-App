f0e80985be9c94931da6f79e76bf0c8720337899
Octavia Suceava <72067315+917OctaviaSuceava@users.noreply.github.com>
2023-04-12 11:45:26 +0300

    gets data

--- front/weather-app/src/components/searchComponent.js
+++ front/weather-app/src/components/searchComponent.js
@@ -1,12 +1,89 @@
 import React, { useState } from "react";
-//import "../styles/searchStyle";
+import axios from 'axios';
 
 function SearchComp() {
     const [searchString, setSearchString] = useState("");
+    const [cityData, setCityData] = useState(null);
+    const [latitude, setLatitude] = useState("");
+    const [longitude, setLongitude] = useState("");
+    const [cityName, setCityName] = useState("");
+    const [country, setCountry] = useState("");
 
-    const handleSearchClick= () => {
+    /*const handleSearchClick= (e) => {
+        e.preventDefault();
+        handleSubmit();
+        console.log("HEREEE", latitude, longitude);
+        sendCityData();
+        getCityData();
         console.log('search');
+    }*/
+
+    
+    const handleSearchClick= async (e) => {
+        e.preventDefault();
+        await handleSubmit();
+        console.log("HEREEE", latitude, longitude);
+        sendCityData();
+        getCityData();
+        console.log('search');
+        };
+
+    
+    
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '7b29a86305msheae3d4bab2f1105p1997c2jsn34379b010cce',
+            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+        }
+    };
+
+    
+    const handleSubmit = () => {
+        console.log(searchString);
+        return fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${searchString}`, options)
+	            .then(response => response.json())
+	            .then(response =>{
+                    return {
+                        options: response.data.map((city) => {
+                            setLatitude(city.latitude);
+                            setLongitude(city.longitude);
+                            setCityName(city.name);
+                            setCountry(city.country);
+                            console.log(city.name);
+                            return {
+                                latitude: city.latitude,
+                                longitude: city.longitude,
+                                name: city.name,
+                                country: city.country
+                            }
+                        })
+                    }
+                    })
+                    .catch(err => console.error(err));
     }
+
+    const sendCityData = () => {    
+        axios.post('http://localhost:3001/cityData', 
+        {latitude, longitude})
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    const getCityData = () => {
+        axios.get('http://localhost:3001/getData')
+            .then((response) => {
+                console.log(response.data);
+                setCityData(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }//<p>{cityData?.main.temp}</p>
     
     return (
         <div>
@@ -16,8 +93,12 @@ function SearchComp() {
                     onChange={(e) => setSearchString(e.target.value)}
                     style={{borderRadius: 5, borderColor: "grey", width: 550, height: 40, textIndent: 4, fontSize: 16}}/>
                 <br></br>
-            {/*<button type="submit" className='-ml-9 mt-4'><img src={search_icon} alt='search'/></button>*/}
-            </form>
+            <button type="submit">GO</button> 
+        </form>
+
+        <div>
+            <p>{cityData?.main.temp}</p>
+        </div>
         </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,8 +1,58 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
+app.use(express.json());
+app.use(cors());
+const axios = require('axios');
+// default: 2ba33f47fe046f77711ed2800628f2b6
+// f3fc5a8c5b60c4788393c60f5d83ddcd
+var API_KEY = '2ba33f47fe046f77711ed2800628f2b6'; //
+var URL = 'https://api.openweathermap.org/data/2.5/';
+
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.options('*', function (req, res) {
+  res.sendStatus(200);
+});
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+let lat;
+let lon;
+
+app.post('/cityData', (req, res) => {
+  //console.log(req.body);
+  //console.log(req.body.latitude); 
+  //console.log(req.body.longitude);
+  lat = req.body.latitude;
+  lon = req.body.longitude;
+  res.send('Data received!');
+});
+
+app.get('/getData', async (req, res) => {
+    let result;
+    try {
+        console.log(lat); 
+        console.log(lon);
+        result = await axios.get(URL + "weather?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY + "&units=metric"); //
+        console.log(result);
+        //res.send(res.json(result.data));
+        res.json(result.data);
+
+    } catch (err) {
+        console.error(`failed all api due to ${err.message}`);
+    }
+});
+
+
 app.listen(3001, function () {
   console.log('Example app listening on port 3001!');
 });
